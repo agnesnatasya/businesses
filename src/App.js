c512719c5302e27454b2122de7cd0ca50315e7a3
agnesnatasya <e0313565@u.nus.edu>
2020-05-22 20:20:04 +0800

    Setup the home page

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>businesses</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
