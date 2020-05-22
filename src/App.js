5ab681e517769dba0de9d36444c80cbeadb72881
agnesnatasya <e0313565@u.nus.edu>
2020-05-22 14:59:30 +0800

    Add business list and search bar components in app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>businesses</h1>
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
