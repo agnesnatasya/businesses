b0bbdf4b61948450b63109a422f8427d8b31655c
agnesnatasya <e0313565@u.nus.edu>
2020-05-25 20:21:33 +0800

    Change the title

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>business</h1>
+        <h1>businesses</h1>
           <SearchBar searchYelp={this.searchYelp} />
           <BusinessList businesses={this.state.businesses} />
       </div>
